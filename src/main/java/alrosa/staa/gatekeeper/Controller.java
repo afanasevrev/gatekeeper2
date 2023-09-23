a53b6f9ed15dbe149a39f094110c5fb1ebfe8916
afanasevrev <rewoly@mail.ru>
2023-09-23 20:10:00 +0900

    initial commit

--- src/main/java/alrosa/staa/gatekeeper/Controller.java
+++ src/main/java/alrosa/staa/gatekeeper/Controller.java
@@ -3,10 +3,13 @@ package alrosa.staa.gatekeeper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -17,41 +20,52 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
     @FXML
+    private AnchorPane anchorPane = new AnchorPane();
+    //Добавялем вертикальный сплиттер
+    private SplitPane splitVertical = new SplitPane();
+    //
+    //Добавляем корень дерева
     private TreeView<String> root = new TreeView<String>();
-
-    private ArrayList<TreeItem> objects = new ArrayList<TreeItem>();
-
+    //Добавляем главное дерево
     private TreeItem<String> mainSystem = new TreeItem<String>("Главный");
-    private TreeItem<String> server = new TreeItem<String>("Сервер");
-    private TreeItem<String> controller = new TreeItem<String>("Контроллер");
-
-    public void setObject(String pic, TreeItem item) {
-        Circle circle =new Circle(30);
-        Image image = new Image(pic);
-        ImageView imageView = new ImageView(image);
-        imageView.setFitWidth(20);
-        imageView.setFitHeight(20);
-        imageView.setPreserveRatio(true);
-        imageView.setClip(circle);
-
-        item.setGraphic(imageView);
-    }
+    //Добавляем верхнее окно
+    private AnchorPane windowUp = new AnchorPane();
+    //Добавляем нижнее окно
+    private AnchorPane windowDown = new AnchorPane();
+    //Добавляем горизонтальный сплиттер
+    private SplitPane splitHorizontal = new SplitPane();
+    //Добавляем окно для дерева
+    private AnchorPane windowTree = new AnchorPane();
+    //Добавляем окно для объектов
+    private AnchorPane windowObjects = new AnchorPane();
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        objects.add(new TreeItem<String>("Сервер"));
-        objects.add(new TreeItem<String>("Контроллер"));
-        for (TreeItem item: objects) {
-            setObject("main.png", item);
-        }
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        //Привязываем сплиттер к окну так, чтобы он растягивался вместе с окном
+       AnchorPane.setBottomAnchor(splitVertical, 0.0);
+       AnchorPane.setLeftAnchor(splitVertical, 0.0);
+       AnchorPane.setRightAnchor(splitVertical, 0.0);
+       AnchorPane.setTopAnchor(splitVertical, 0.0);
+       //Объявляем, что наш сплиттер вертикальный
+       splitVertical.setOrientation(Orientation.VERTICAL);
+       //Добавляем к сплиттеру левый и правый окна
+       splitVertical.getItems().addAll(windowUp, windowDown);
+        //Добавляем к окну вертикальный сплиттер
+       anchorPane.getChildren().addAll(splitVertical);
+       //Объявляем, что наш второй сплиттер горизонтальный
+        splitHorizontal.setOrientation(Orientation.HORIZONTAL);
+        //Привязываем горизонтальный сплиттер к окну так, чтобы он растягивался вместе с окном
+        AnchorPane.setBottomAnchor(splitHorizontal, 0.0);
+        AnchorPane.setLeftAnchor(splitHorizontal, 0.0);
+        AnchorPane.setRightAnchor(splitHorizontal, 0.0);
+        AnchorPane.setTopAnchor(splitHorizontal, 0.0);
+        //К верхнему окну добавляем горизонтальный сплиттер
+        windowUp.getChildren().addAll(splitHorizontal);
+       //К горизонтальному спиттеру добавляем два окна, для дерева и объектов
+        splitHorizontal.getItems().addAll(windowTree, windowObjects);
 
-      //  mainSystem.setGraphic(imageView);
-      //  server.setGraphic(imageViewServer);
-      //  controller.setGraphic(imageViewController);
-      //  mainSystem.getChildren().add(server);
-      //  server.getChildren().add(controller);
-
-        root.setRoot(mainSystem);
     }
 }
--- src/main/java/alrosa/staa/gatekeeper/GateKeeper.java
+++ src/main/java/alrosa/staa/gatekeeper/GateKeeper.java
@@ -12,7 +12,7 @@ public class GateKeeper extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GateKeeper.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Gate keeper");
+        stage.setTitle("Gate Keeper Access Control");
         stage.setScene(scene);
         stage.show();
     }
