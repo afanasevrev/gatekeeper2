5a378ac87d36e7ec42a36dc40a6d9335123be4bd
unknown <rewoly@mail.ru>
2023-09-28 11:58:16 +0900

    Work in the console

---
+++ src/main/java/alrosa/staa/gatekeeper/objects/Computer.java
@@ -0,0 +1,40 @@
+package alrosa.staa.gatekeeper.objects;
+
+public class Computer extends MainObject {
+    private String name;
+    private String ipAddress;
+
+    public Computer() {
+        this.name = "Компьютер";
+        this.ipAddress = "0.0.0.0";
+    }
+    public Computer(String name) {
+        this.name = name;
+        this.ipAddress = "0.0.0.0";
+    }
+    public Computer(String name,String ipAddress) {
+        this.name = name;
+        this.ipAddress = ipAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "name";
+    }
+}
--- src/main/java/alrosa/staa/gatekeeper/objects/Server.java
+++ src/main/java/alrosa/staa/gatekeeper/objects/Server.java
@@ -9,28 +9,22 @@ public class Server extends MainObject {
         this.name = "Сервер";
         this.ipAddress = "0.0.0.0";
     }
-
     public Server(String name) {
         this.name = name;
         this.ipAddress = "0.0.0.0";
     }
-
     public Server(String name, String ipAddress) {
         this.name = name;
         this.ipAddress = ipAddress;
     }
-
     public void setIpAddress(String ipAddress) {
         this.ipAddress = ipAddress;
     }
-
     public String getIpAddress() {
         return ipAddress;
     }
-
     @Override
     public String toString() {
         return name;
     }
-
 }
