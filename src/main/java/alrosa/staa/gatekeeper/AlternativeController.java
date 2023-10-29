da3f789fe2d2d943474c1001ada288bfa6a3b3cd
afanasevrev <rewoly@mail.ru>
2023-10-30 00:34:14 +0900

    work on the console

--- src/main/java/alrosa/staa/gatekeeper/AlternativeController.java
+++ src/main/java/alrosa/staa/gatekeeper/AlternativeController.java
@@ -10,35 +10,53 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AlternativeController implements Initializable {
+    //Окно для кнопок
     @FXML
     private AnchorPane anchorPaneForButtons = new AnchorPane();
+    //Окно для дерева
+    @FXML
+    private AnchorPane anchorPaneForItems = new AnchorPane();
+    //Окно для объектов
+    @FXML
+    private AnchorPane anchorPaneForObjects = new AnchorPane();
+    //Окно для журнала событий
+    @FXML
+    private AnchorPane anchorPaneForLogs = new AnchorPane();
+    //Вертикальный сплиттер
     @FXML
     private SplitPane splitPaneVertical = new SplitPane();
+    //Горизонтальный сплиттер
     @FXML
     private SplitPane splitPaneHorizontal = new SplitPane();
+    //Кнопка создать
     @FXML
     private Button apply = new Button();
+    //Кнопка отменить
     @FXML
     private Button cancel = new Button();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Прикрепляем вертикальный сплиттер к окну так, чтобы он растягивался вместе с ним
         AnchorPane.setLeftAnchor(splitPaneVertical, 0.0);
         AnchorPane.setBottomAnchor(splitPaneVertical, 0.0);
         AnchorPane.setRightAnchor(splitPaneVertical, 0.0);
         AnchorPane.setTopAnchor(splitPaneVertical, 0.0);
-
+        //Прикрепляем горизонтальный сплиттер к окну так, чтобы оп растягивался вместе с ним
         AnchorPane.setLeftAnchor(splitPaneHorizontal, 0.0);
         AnchorPane.setBottomAnchor(splitPaneHorizontal, 0.0);
         AnchorPane.setRightAnchor(splitPaneHorizontal, 0.0);
         AnchorPane.setTopAnchor(splitPaneHorizontal, 0.0);
-
+        //Прикрепляем кнопки к правому и нижнему краю окна
+        AnchorPane.setRightAnchor(apply, 180.0);
+        AnchorPane.setRightAnchor(cancel, 80.0);
         AnchorPane.setBottomAnchor(apply, 10.0);
         AnchorPane.setBottomAnchor(cancel, 10.0);
-
+        //Прикрепляем окно для кнопок к краям
         AnchorPane.setLeftAnchor(anchorPaneForButtons, 0.0);
         AnchorPane.setBottomAnchor(anchorPaneForButtons, 0.0);
         AnchorPane.setRightAnchor(anchorPaneForButtons, 0.0);
         AnchorPane.setTopAnchor(anchorPaneForButtons, 0.0);
+
     }
 }
--- src/main/java/alrosa/staa/gatekeeper/GateKeeper.java
+++ src/main/java/alrosa/staa/gatekeeper/GateKeeper.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 public class GateKeeper extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-       // FXMLLoader fxmlLoader = new FXMLLoader(GateKeeper.class.getResource("main.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(GateKeeper.class.getResource("main.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(GateKeeper.class.getResource("alternativemain.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        //scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
         stage.setTitle("Gate Keeper Secure Access");
         stage.setScene(scene);
         stage.show();
