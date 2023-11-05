085c513a9503a5c2dd37ccd19bb2b8a8cdc90b75
afanasevrev <rewoly@mail.ru>
2023-11-06 03:31:58 +0900

    work on the console

--- src/main/java/alrosa/staa/gatekeeper/AlternativeController.java
+++ src/main/java/alrosa/staa/gatekeeper/AlternativeController.java
@@ -1,17 +1,16 @@
 package alrosa.staa.gatekeeper;
 
+import alrosa.staa.gatekeeper.containers.*;
 import alrosa.staa.gatekeeper.objects.Direction;
 import alrosa.staa.gatekeeper.objects.global.Global;
 import alrosa.staa.gatekeeper.objects.mainsystem.MainSystem;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -41,14 +40,96 @@ public class AlternativeController implements Initializable {
     //Кнопка отменить
     @FXML
     private Button cancel = new Button();
-    //Главный объект на дереве
-    private TreeItem<Global> mainSystem = new TreeItem<>(new MainSystem());
     //Указываем путь к рисунку главного дерева в системе
     private Image imageMain = new Image("main.png");
+
     //Создаем ImageView для рисунка main.png
     private ImageView imageViewMain = new ImageView(imageMain);
+    //Указываем путь к рисунку сервер
+    private final Image imageServer = new Image("server.png");
+    //Указываем путь к рисунку компьютер
+    private final Image imageComputer = new Image("computer.png");
+    //Указываем путь к рисунку бюро
+    private final Image imageBureau = new Image("bureau.png");
+    //Указываем путь к рисунку perco
+    private final Image imagePerco = new Image("perco.png");
+    //Указываем путь к рисунку PERCoC01
+    private final Image imagePERCoC01 = new Image("PERCoC01.png");
+    //Указываем путь к рисунку cardreader
+    private final Image imageCardReader = new Image("cardreader.png");
+    //Указываем путь к рисунку users
+    private final Image imageUsers = new Image("users.png");
+    //Указываем путь к рисунку admins
+    private final Image imageAdmins = new Image("admins.png");
+    //Указываем путь к рисунку operators
+    private final Image imageOperators = new Image("operators.png");
+    //Указываем путь к рисунку cards
+    private final Image imageCards = new Image("cards.png");
+    //Указываем путь к рисунку макеты карт
+    private final Image imageCardLayouts = new Image("card_layouts.png");
+    //Указываем путь к рисунку global_access_levels
+    private final Image imageGlobalAccessLevels = new Image("global_access_levels.png");
+    //Указываем путь к рисунку positions
+    private final Image imagePositions = new Image("positions.png");
+    //Указываем путь к рисунку organizations
+    private final Image imageOrganizations = new Image("organizations.png");
+    //Указываем путь к рисунку man
+    private final Image imageMan = new Image("man.png");
+    //Указываем путь к рисунку woman
+    private final Image imageWoman = new Image("woman.png");
+    //Указываем путь к рисунку card
+    private final Image imageCard = new Image("card.png");
+    //Указываем путь к рисунку card_layout
+    private final Image imageCardLayout = new Image("card_layout.png");
+    //Указываем путь к рисунку global_access_level
+    private final Image imageGlobalAccessLevel = new Image("global_access_level.png");
+    //Указываем путь к рисунку position
+    private final Image imagePosition = new Image("position.png");
+    //Указываем путь к рисунку organization
+    private final Image imageOrganization = new Image("organization.png");
+    //Указываем путь к рисунку office
+    private final Image imageOffice = new Image("office.png");
+    //Указываем путь к рисунку console
+    private final Image imageConsole = new Image("console.png");
+    //Добавим контекстное меню
+    private ContextMenu contextMenu = new ContextMenu();
+    //Создание кнопки "Добавить"
+    private MenuItem menuAdd = new MenuItem("Добавить");
+    //Создание кнопки "Удалить"
+    private MenuItem menuDelete = new MenuItem("Удалить");
+    //Главный объект на дереве
+    private TreeItem<Global> mainSystem = new TreeItem<>(new MainSystem());
     //Дерево
     private TreeView root = new TreeView(mainSystem);
+    //Создаем экземпляр главного контейнера
+    private MainContainer mainContainer = new MainContainer();
+    //Создаем экземпляр для серверного контейнера
+    private ServerContainer serverContainer = new ServerContainer();
+    //Создаем экземпляр для контейнера оборудования Perco
+    private PercoContainer percoContainer = new PercoContainer();
+    //Создаем экземпляр для контейнера Считывателя
+    private CardReaderContainer cardReaderContainer = new CardReaderContainer();
+    //Создаем экземпляр для контейнера Бюро
+    private BureauContainer bureauContainer = new BureauContainer();
+    //Создаем экземпляр для контейнера Пользователи/Администраторы/Операторы
+    private UsersContainer usersContainer = new UsersContainer();
+    //Создаем экземпляр для контейнера Карты доступа
+    private CardsContainer cardsContainer = new CardsContainer();
+    //Создаем экземпляр для контейнера Макеты карт
+    private CardLayoutsContainer cardLayoutsContainer = new CardLayoutsContainer();
+    //Создаем экземпляр для контейнера Глобальные уровни доступа
+    private GlobalAccessLevelsContainer globalAccessLevelsContainer = new GlobalAccessLevelsContainer();
+    //Создаем экземпляр для контейнера Должности
+    private PositionsContainer positionsContainer = new PositionsContainer();
+    //Создаем экземпляр для контейнера Организации
+    private OrganizationsContainer organizationsContainer = new OrganizationsContainer();
+    //Создаем экземпляр для контейнера Отдела
+    private OrganizationContainer organizationContainer = new OrganizationContainer();
+    //Создаем экземпляр для контейнера Компьютер
+    private ComputerContainer computerContainer = new ComputerContainer();
+    //Добавляем сцену
+    private Stage stage = new Stage();
+    //Добавляем статический объект дерева
     private static TreeItem<Global> item;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -82,6 +163,11 @@ public class AlternativeController implements Initializable {
         imageViewMain.setFitWidth(25);
         imageViewMain.setFitHeight(25);
         mainSystem.setGraphic(imageViewMain);
+
+        //В контекстное меню добавляем кнопки
+        contextMenu.getItems().addAll(menuAdd, menuDelete);
+        //В наше дерево добавляем контекстное меню
+        root.setContextMenu(contextMenu);
     }
     private void addItem(TreeItem treeItem, Image image) {
         ImageView imageView = new ImageView(image);
